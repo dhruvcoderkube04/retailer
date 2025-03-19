898883b282ac43e211c68b14ac5908516cb5544a
darkcoder0 <darkcoder0@outlook.com>
2025-03-20 00:03:33 +0530

    fix:route middle ware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,10 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
-            'admin' => AdminMiddleware::class,
-            'wholesaler' => WholesalerMiddleware::class,
             'retailer' => RetailerMiddleware::class,
-            'validate.api' => ValidateApiKey::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
