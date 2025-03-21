58a62a8d1350fbb7c82d6bd642b1484f27e68d90
darkcoder0 <darkcoder0@outlook.com>
2025-03-22 01:23:48 +0530

    Remov: - removeasset unnecessary assets - fix profile

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -14,7 +14,15 @@
         <!--begin::Mobile logo-->
         <div class="d-flex align-items-center flex-grow-1 flex-lg-grow-0">
             <a href="index.html" class="d-lg-none">
-                <img alt="Logo" height="20" src="assets/media/logos/default-small.svg" class="h-30px" />
+                @php
+                    $logoPath = 'uploads/company_profile/' . (Auth::user()->userDetail->company_logo ?? '');
+                    $logoUrl = file_exists(public_path($logoPath)) && Auth::user()->userDetail && Auth::user()->userDetail->company_logo
+                        ? asset($logoPath)
+                        : asset('assets/media/avatars/no-profile.png'); // Default image path
+                @endphp
+                <img src="{{ $logoUrl }}" class="rounded-3" alt="user"  height="20" />
+                <br />
+                {{Auth::user()->firstname}}
             </a>
         </div>
         <!--end::Mobile logo-->
@@ -112,10 +120,7 @@
                 <div class="app-navbar-item ms-1 ms-md-4" id="kt_header_user_menu_toggle">
                     <!--begin::Menu wrapper-->
                     <div class="cursor-pointer symbol symbol-35px" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
-                        <img src="{{ Auth::user()->userDetail && Auth::user()->userDetail->company_logo
-                        ? asset('uploads/company_profile/' . Auth::user()->userDetail->company_logo)
-                        : asset('assets/media/avatars/no-profile.png') }}"
-                            class="rounded-3" alt="user" />
+                        <img src="{{ $logoUrl }}" class="rounded-3" alt="user"   />
                     </div>
                     <!--begin::User account menu-->
                     <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-color fw-semibold py-4 fs-6 w-275px" data-kt-menu="true">
@@ -124,10 +129,7 @@
                             <div class="menu-content d-flex align-items-center px-3">
                                 <!--begin::Avatar-->
                                 <div class="symbol symbol-50px me-5">
-                                    <img src="{{ Auth::user()->userDetail && Auth::user()->userDetail->company_logo
-                                    ? asset('uploads/company_profile/' . Auth::user()->userDetail->company_logo)
-                                    : asset('assets/media/avatars/no-profile.png') }}"
-                                        class="rounded-3" alt="user" />
+                                    <img src="{{ $logoUrl }}" class="rounded-3" alt="user"/>
                                 </div>
                                 <!--end::Avatar-->
                                 <!--begin::Username-->
