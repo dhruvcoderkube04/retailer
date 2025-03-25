56a8689d04138d852b8c8ac4086336a54add72e1
unknown <pratikginoya194@gmail.com>
2025-03-25 21:27:49 +0530

    add encryption and decryption function in helper.php

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -21,4 +21,20 @@
 //     }
 // }
 
+if (!function_exists('encryptId')) {
+   function encryptId($id)
+   {
+      $key = 'ZLCvrkbzipyCbam8Up+xWSo2Ky7GtBFZkEQWcIYYGM0=';
+      return base64_encode(openssl_encrypt($id, "AES-256-CBC", $key, 0, substr(hash('sha256', $key), 0, 16)));
+   }
+}
+
+if (!function_exists('decryptId')) {
+   function decryptId($encryptedId)
+   {
+      $key = 'ZLCvrkbzipyCbam8Up+xWSo2Ky7GtBFZkEQWcIYYGM0=';
+      return openssl_decrypt(base64_decode($encryptedId), "AES-256-CBC", $key, 0, substr(hash('sha256', $key), 0, 16));
+   }
+}
+
 ?>
