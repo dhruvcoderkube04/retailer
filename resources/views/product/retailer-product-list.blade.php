e9246d6ee0f15f0b4ec888cf56764e77bacddd3f
unknown <pratikginoya194@gmail.com>
2025-03-29 08:26:27 +0530

    add home page route in all pages

--- resources/views/product/retailer-product-list.blade.php
+++ resources/views/product/retailer-product-list.blade.php
@@ -23,7 +23,7 @@
                         <ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-1 pt-1">
                             <!--begin::Item-->
                             <li class="breadcrumb-item text-muted">
-                                <a href="index.html" class="text-muted text-hover-primary">Home</a>
+                                <a href="{{route('retailer.dashboard')}}" class="text-muted text-hover-primary">Home</a>
                             </li>
                             <!--end::Item-->
                             <!--begin::Item-->
