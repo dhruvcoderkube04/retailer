daebf10d50239e390119db9330093bb1faf13f09
Karan <karanjagtap.coderkube@gmail.com>
2025-08-05 15:50:42 +0530

    bugfix

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -211,11 +211,9 @@
                 </div>
                 <!--end::Theme mode-->
                 <!--begin::User menu-->
-                <div class="app-navbar-item ms-1 ms-md-4" id="kt_header_user_menu_toggle">
+                <div class="app-navbar-item ms-1 ms-md-4" id="kt_header_user_menu_toggle" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
                     <!--begin::Menu wrapper-->
-                    <div class="cursor-pointer symbol symbol-35px"
-                        data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent"
-                        data-kt-menu-placement="bottom-end">
+                    <div class="cursor-pointer symbol symbol-35px">
                         <img src="{{ $logoUrl }}" class="rounded-3" alt="user" />
                     </div>
                     <a href="{{ route('retailer.profile.details') }}">
@@ -223,52 +221,52 @@
                             {{ Auth::user()->userDetail->company_name }}
                         </div>
                     </a>
-
-                    <!--begin::User account menu-->
-                    <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-color fw-semibold py-4 fs-6 w-275px"
-                        data-kt-menu="true">
-                        <!--begin::Menu item-->
-                        <div class="menu-item px-3">
-                            <div class="menu-content d-flex align-items-center px-3">
-                                <!--begin::Avatar-->
-                                <div class="symbol symbol-50px me-5">
-                                    <img src="{{ $logoUrl }}" class="rounded-3" alt="user" />
-                                </div>
-                                <!--end::Avatar-->
-                                <!--begin::Username-->
-                                <div class="d-flex flex-column">
-                                    <div class="fw-bold d-flex align-items-center fs-5"> {{ Auth::user()->userDetail->company_name }}
-                                        <span class="badge badge-light-success fw-bold fs-8 px-2 py-1 ms-2"></span>
-                                    </div>
-                                    <a href="#"
-                                        class="fw-semibold text-muted text-hover-primary fs-7">{{ Auth::user()->email }}</a>
-                                </div>
-                                <!--end::Username-->
-                            </div>
-                        </div>
-                        <!--end::Menu item-->
-                        <!--begin::Menu separator-->
-                        <div class="separator my-2"></div>
-                        <!--end::Menu separator-->
-                        <!--begin::Menu item-->
-                        <div class="menu-item px-5">
-                            <a href="{{ route('retailer.profile.details') }}" class="menu-link px-5">My Profile</a>
-                        </div>
-                        <!--end::Menu item-->
-                        <!--begin::Menu item-->
-                        <div class="menu-item px-5">
-                            <form action="{{ route('retailer.logout') }}" method="post" class="d-inline">
-                                @csrf
-                                <button type="submit" class="menu-link px-5 border-0  bg-transparent">Sign
-                                    Out</button>
-                            </form>
-                        </div>
-                        <!--end::Menu item-->
-                    </div>
-                    <!--end::User account menu-->
-                    <!--end::Menu wrapper-->
                 </div>
                 <!--end::User menu-->
+                <!--begin::User account menu-->
+                <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-color fw-semibold py-4 fs-6 w-275px"
+                    data-kt-menu="true">
+                    <!--begin::Menu item-->
+                    <div class="menu-item px-3">
+                        <div class="menu-content d-flex align-items-center px-3">
+                            <!--begin::Avatar-->
+                            <div class="symbol symbol-50px me-5">
+                                <img src="{{ $logoUrl }}" class="rounded-3" alt="user" />
+                            </div>
+                            <!--end::Avatar-->
+                            <!--begin::Username-->
+                            <div class="d-flex flex-column">
+                                <div class="fw-bold d-flex align-items-center fs-5"> {{ Auth::user()->userDetail->company_name }}
+                                    <span class="badge badge-light-success fw-bold fs-8 px-2 py-1 ms-2"></span>
+                                </div>
+                                <a href="#"
+                                    class="fw-semibold text-muted text-hover-primary fs-7">{{ Auth::user()->firstname }} {{ Auth::user()->lastname }}</a>
+                            </div>
+                            <!--end::Username-->
+                        </div>
+                    </div>
+                    <!--end::Menu item-->
+                    <!--begin::Menu separator-->
+                    <div class="separator my-2"></div>
+                    <!--end::Menu separator-->
+                    <!--begin::Menu item-->
+                    <div class="menu-item px-5">
+                        <a href="{{ route('retailer.profile.details') }}" class="menu-link px-5">My Profile</a>
+                    </div>
+                    <!--end::Menu item-->
+                    <!--begin::Menu item-->
+                    <div class="menu-item px-5">
+                        <form action="{{ route('retailer.logout') }}" method="post" class="d-inline">
+                            @csrf
+                            <button type="submit" class="menu-link px-5 border-0  bg-transparent">Sign
+                                Out</button>
+                        </form>
+                    </div>
+                    <!--end::Menu item-->
+                </div>
+                <!--end::User account menu-->
+                <!--end::Menu wrapper-->
+
             </div>
             <!--end::Navbar-->
         </div>
