038032a65b2ea7a82110dfe09627de277d2acd53
Dhaval-patel27 <pateldhaval.coderkube@gmail.com>
2025-08-14 15:11:05 +0530

    click on calender icon not open calender - resolve

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -540,6 +540,10 @@
 
         cb(start, end);
 
+        $('.ki-calendar-8').on('click', function () {
+            $('#kt_daterangepicker_4').trigger('click');
+        });
+
 
         $(document).on('change', '#kt_daterangepicker_4', function () {
             const fullDate = $(this).val();
