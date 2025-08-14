9d2ec846692b4a192bfc2b79217592f70b65d29b
Dhaval-patel27 <pateldhaval.coderkube@gmail.com>
2025-08-14 15:02:42 +0530

    Suggestion Category List in home redirect error resolve

--- resources/views/category/suggestion-box.blade.php
+++ resources/views/category/suggestion-box.blade.php
@@ -21,7 +21,7 @@
                             <ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-0 pt-1">
                                 <!--begin::Item-->
                                 <li class="breadcrumb-item text-muted">
-                                    <a href="#" class="text-muted text-hover-primary">Category</a>
+                                    <a href="{{ route('retailer.dashboard') }}" class="text-muted text-hover-primary">Home</a>
                                 </li>
                                 <!--end::Item-->
                                 <!--begin::Item-->
