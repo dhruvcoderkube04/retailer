cf54e993c4c3b7f57aaec2936080dbe5c3ed0bef
iftekhar <iftekhar.coderkube@gmail.com>
2025-08-28 18:50:12 +0530

    TT-564

--- app/Http/Controllers/RetailerCategoryController.php
+++ app/Http/Controllers/RetailerCategoryController.php
@@ -18,6 +18,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\Rule;
 
 class RetailerCategoryController extends Controller
 {
@@ -426,7 +427,7 @@ class RetailerCategoryController extends Controller
                     'min:2',
                     'max:255',
                     'regex:/^[A-Za-z\s_-]+$/',
-                    new NoCodeInjection
+                    new NoCodeInjection,
                 ],
                 'subCategoryName' => [
                     'required',
@@ -434,8 +435,13 @@ class RetailerCategoryController extends Controller
                     'min:2',
                     'max:255',
                     'regex:/^[A-Za-z\s_-]+$/',
-                    new NoCodeInjection
+                    new NoCodeInjection,
+                    Rule::unique('category_suggestions', 'sub_category_name')->where(function ($query) use ($request) {
+                        return $query->where('category_name', $request->categoryName);
+                    }),
                 ],
+            ], [
+                'subCategoryName.unique' => 'This category and subcategory suggestion already exists.'
             ]);
 
             $user = Auth::user();
